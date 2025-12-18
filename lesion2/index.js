655ac629a089a58c7075ad8cf9daf2f270d9719d
Bùi Quang Hà <ha@zework.com>
2025-12-18 13:34:58 +0700

    lam 1 / 3 lession 2

---
+++ lesion2/index.js
@@ -0,0 +1,78 @@
+import {
+    sumArray,
+    countOccurrences, 
+    removeDuplicates,
+    flattenArray,
+    isSymmetric,
+    findSecondLargest,
+    sortProductsByPrice,
+    findMostExpensiveProduct,
+    groupByType,isSubset,
+    findMaxKey,
+    mergeObjectsSumValues,
+    countElements,
+    cleanObject,
+    hasDuplicate
+
+ } from "./until.js"
+
+// let arr = [1, 2, 3, 4]
+// const bai1 = sumArray(arr) 
+// console.log(bai1)
+
+// let arr = [1, 2, 2, 3, 2]
+// let value = 2
+// console.log(countOccurrences(arr, value))
+
+// let arr = [1, 2, 2, 3, 4, 4, 6, 2, 3, 6]
+// const bai2 = removeDuplicates(arr)
+// console.log(bai2)
+
+// let arr = [[1, 2], [3, 4]];
+// const bai4 = flattenArray(arr);
+// console.log(bai4);
+
+// let arr = [1, 2, 3, 2, 1, 0, 4]
+// console.log(isSymmetric(arr))
+
+// let arr = [3, 1, 4, 2, 4]
+// console.log(findSecondLargest(arr))
+
+// let products = [
+//     { name: 'Product A', price: 30 },
+//     { name: 'Product B', price: 20 },
+//     { name: 'Product C', price: 50 }
+// ];
+
+// console.log(sortProductsByPrice(products))
+// console.log(findMostExpensiveProduct(products))
+
+
+// let arr = [
+//     { type: 'fruit', name: 'apple' },
+//     { type: 'vegetable', name: 'carrot' },
+//     { type: 'fruit', name: 'banana' }
+// ];
+
+// console.log(groupByType(arr))
+
+// let arr1 = [1, 2, 3, 4]
+// let arr2 = [2, 4]
+// console.log(isSubset(arr1, arr2))
+
+// let obj = { a: 10, b: 20, c: 40 }
+
+// console.log(findMaxKey(obj))
+
+// let obj1 = { a: 10, b: 20 };
+// let obj2 = { b: 30, c: 40 };
+// console.log(mergeObjectsSumValues(obj1, obj2))
+
+// let arr = ['a', 'b', 'a', 'c', 'b', 'a']
+// console.log(countElements(arr))
+
+// let obj = { a: 1, b: null, c: undefined, d: 4 } 
+// console.log(cleanObject(obj))
+
+let arr = [1,2,3,4,5,1]
+console.log(hasDuplicate(arr))
---
+++ lesion2/until.js
@@ -0,0 +1,155 @@
+export const sumArray = (arr) => {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i]
+    }
+    return result
+}
+
+export const countOccurrences = (arr, value) => {
+    let result = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == value) {
+            result++
+        }
+    }
+    return result
+}
+
+export function removeDuplicates(arr) {
+    let result = []
+
+    const objMap = {};
+
+    for (let i = 0; i < arr.length; i++) {
+        if(objMap[arr[i]] === undefined){
+            objMap[arr[i]] = 1
+        }
+    }   
+    result = Object.keys(objMap)
+    return result
+}
+
+export function flattenArray(arr){
+    let res = []
+    for(let i = 0; i < arr.length; i++){
+        for( let j = 0; j < arr[i].length; j++){
+            res.push(arr[i][j])
+        }
+    }
+    return res
+}   
+
+export function isSymmetric(arr){
+    let left = 0
+    let right = arr.length-1
+    while(left < right){
+        if(arr[left] != arr[right]){
+            return false
+        }
+        left ++
+        right --
+    }
+    return true
+}
+
+export function findSecondLargest(arr){
+    let max = arr[0]
+    let secondMax = arr[0]
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            secondMax = max
+            max = arr[i]
+        }
+        else if (arr[i] < max && arr[i] > secondMax){
+            secondMax = arr[i]
+        }
+    }
+    return secondMax
+}
+
+export function sortProductsByPrice(products){
+    return products.sort((a, b) => a.price - b.price)
+}
+
+export function findMostExpensiveProduct(products){
+    let max = products[0]
+    for(let i = 1; i < products.length; i++){
+        if(products[i].price > max.price){
+            max = products[i]
+        }
+    }
+    return max
+}
+
+export function groupByType(arr){
+    let res = {}
+
+    for( let i = 0; i < arr.length; i++){
+        const {type, name} = arr[i]
+        if(res[type] === undefined){
+            res[type] = [];
+        }
+        res[type].push(name)
+     }
+    return res
+}
+
+export function isSubset(arr1, arr2) {
+    return arr2.filter(x => arr1.includes(x)).length === arr2.length;
+}
+
+export function findMaxKey(obj){
+    return  Object.keys(obj).reduce((maxKey, key) => 
+    obj[key] > obj[maxKey] ? key : maxKey);
+}
+
+export function mergeObjectsSumValues(obj1, obj2){
+    let res = {...obj1}
+    for(let key in obj2){
+        if(res[key] != undefined){
+            res[key] += obj2[key]
+        }
+        else{
+            res[key] = obj2[key]
+        }
+    }
+    return res
+}
+
+export function countElements(arr){
+    let res = {}
+    for(let i = 0; i < arr.length; i++){
+        if( res[arr[i]] === undefined){
+            res[arr[i]] = 1
+        }   
+        else{
+             res[arr[i]] += 1
+        }
+    }
+    return res
+}
+
+export function cleanObject(obj){
+    let res = {}
+
+    for(let key in obj){
+
+        if(obj[key] !== undefined && obj[key] !== null){
+            res[key] = obj[key]
+        }
+      
+    }
+    return res
+}
+
+export function hasDuplicate(arr){
+    for(let i = 0; i< arr.length; i++){
+        for(let j = i+1; j< arr.length; j++){
+            if(arr[i] == arr[j]){
+                return false
+            }
+        }
+    }
+    return true
+}
